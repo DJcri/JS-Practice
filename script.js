e7bb84aaac13528cde1241ee9e773b1b465662f5
DJcri <djcri123@gmail.com>
2020-11-28 19:52:23 -0800

    Added code from part 2 of fundamentals practice

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/* Challenge 1 and 2 (Comparing and outputting BMI values) */
+/*
+// Challenge 1 and 2 (Comparing and outputting BMI values)
 
 let heightMark;
 let heightJohn;
@@ -8,7 +9,7 @@ let bmiMark;
 let bmiJohn;
 let conclusionText;
 
-/* Test Data 1 */
+// Test Data 1
 heightMark = 1.69;
 weightMark = 79;
 
@@ -25,7 +26,7 @@ if (markHigherBMI) {
 }
 console.log(`Mark's BMI (${bmiMark}) is ${conclusionText} John's BMI (${bmiJohn}).`);
 
-/* Test Data 2 */
+// Test Data 2 
 heightMark = 1.88;
 weightMark = 95;
 
@@ -42,12 +43,12 @@ if (markHigherBMI) {
 }
 console.log(`Mark's BMI (${bmiMark}) is ${conclusionText} John's BMI (${bmiJohn}).`);
 
-/* Challenge 3 (Comparing and outputting scores with logic statements) */
+// Challenge 3 (Comparing and outputting scores with logic statements)
 
 const minScore = 100;
 let draw;
 let winner;
-/* Teams: d = Dolphins, k = Koalas */
+// Teams: d = Dolphins, k = Koalas 
 let d_score1 = 97, d_score2 = 112, d_score3 = 101;
 let k_score1 = 109, k_score2 = 95, k_score3 = 106;
 let d_averageScore = (d_score1 + d_score2 + d_score3) / 3;
@@ -70,8 +71,124 @@ else {
     console.log(`No team wins!`)
 }
 
-/* Challenge 4 (Calculate tip depending on bill) */
+// Challenge 4 (Calculate tip depending on bill)
 
 const bill = 430;
 const tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
 console.log(`The bill is $${bill}, the tip is $${tip}, and the total is $${bill + tip}`)
+*/
+
+// Start of fundamentals part 2
+
+`use strict`;
+/*
+// Challenge 1 Use functions to determine winner
+
+const calcAverage = (a, b, c) => (a + b + c) / 3;
+
+function checkWinner() {
+    const dolphins = calcAverage(85, 54, 41)
+    const koalas = calcAverage(23, 34, 27)
+    let winner;
+    if (dolphins >= koalas * 2) {
+        winner = "Dolphins";
+    } else if (koalas >= dolphins * 2) {
+        winner = "Koalas";
+    }
+    if (winner) {
+        console.log(`${winner} win! (${dolphins} vs. ${koalas})`)
+    } else {
+        console.log(`No one wins!`)
+    }
+}
+
+checkWinner();
+
+// Challenge 2 Calculate tip using functions and arrays
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
+}
+
+const bills = [125, 555, 44];
+const tips = [
+    (calcTip(bills[0])),
+    (calcTip(bills[1])),
+    (calcTip(bills[2]))
+];
+const totals = [
+    (tips[0] + bills[0]),
+    (tips[1] + bills[1]),
+    (tips[2] + bills[2])
+];
+
+console.log(tips)
+console.log(totals)
+
+// Object Challenge construct a sentence using an object
+
+const jonas = {
+    firstName: `Jonas`,
+    friends: [`Michael`, `Peter`, `Steven`]
+}
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is ${jonas.friends[0]}`)
+
+// Challenge 3 Use objects to calculate BMIs
+
+const mark = {
+    firstName: `Mark`,
+    lastName: `Miller`,
+    fullName: function () { return `${this.firstName} ${this.lastName}`; },
+    weight: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.weight / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+const john = {
+    firstName: `John`,
+    lastName: `Smith`,
+    fullName: function () { return `${this.firstName} ${this.lastName}`; },
+    weight: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.weight / this.height ** 2;
+        return this.bmi;
+    }
+}
+
+const higherBMI = mark.calcBMI() > john.calcBMI() ? mark : john;
+const lowerBMI = higherBMI === mark ? john : mark;
+
+console.log(`${higherBMI.fullName()}'s BMI (${higherBMI.bmi}) is higher than ${lowerBMI.fullName()}'s BMI (${lowerBMI.bmi})`)
+*/
+// Challenge 4 Calculate tips using loops
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = bill => {
+    let p = bill >= 50 && bill <= 300 ? .15 : .2;
+    return bill * p;
+};
+
+const calcAverage = arr => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    };
+    return sum / arr.length;
+};
+
+for (let i = 0; i < bills.length; i++) {
+    let tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+
+console.log(calcAverage(tips));
+console.log(calcAverage(totals));
